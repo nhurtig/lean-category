23adda9d649c0699dfe3978440ed68c7028c640c
Nat Hurtig <hurtig@cs.washington.edu>
2026-02-06 20:42:54 -0800

    a note on enrichment

---
+++ LeanCategory/BraidGroupoid/Enrich.lean
@@ -0,0 +1,10 @@
+-- idea: what if braids between two layers have TWO labels each -- one for the below layer
+-- and one for the above? Makes projection EZ. No need for enrich. Gotta make sure that the
+-- labels are unique, though... or do we? Or, maybe there are "color changing" morphisms
+-- in the braid goid. They're always one-to-one, so they don't change the number of strands.
+-- But issue: the projection would have to somehow "remember" as it passes through color
+-- changes, which is no good. Double-colored strands sounds good. Would just need special
+-- composition rules for braid-layer interactions. This way, layers get to choose how they
+-- name their adjacent strands.
+
+-- TL;DR no need for enrich!
